215b8f157ef0a5b2a19b4388202c9c8120edf585
dalong chen <dalongtw@outlook.com>
2018-12-08 10:08:49 +0800

    add all

---
+++ gitclone.bat
@@ -0,0 +1,7 @@
+mkdir /p ..\sphinxdoc-test-docs
+echo origin  https://github.com/notmikeb/sphinxdoc-test.git
+cd ..\sphindoc-test-docs
+git clone http://github.com/notmikeb/sphinxdoc-test.git html
+cd html
+del .git/index
+git clean -fdx
