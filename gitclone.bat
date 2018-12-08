13d0e2fc7156f9558b18cac0a49503196a47ce5c
dalong chen <dalongtw@outlook.com>
2018-12-09 00:30:17 +0800

    fix

--- gitclone.bat
+++ gitclone.bat
@@ -3,5 +3,5 @@ echo origin  https://github.com/notmikeb/sphinxdoc-test.git
 cd ..\sphinxdoc-test-docs
 git clone http://github.com/notmikeb/sphinxdoc-test.git html
 cd html
-del .git\index
-git clean -fdx
+rem del .git\index
+rem git clean -fdx
